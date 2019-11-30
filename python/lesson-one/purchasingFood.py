4ebbc7aab08d43693d4213db50767761eebd8a83
Jason Carney <jrcarney.16@gmail.com>
2019-11-30 22:12:07 +0000

    Initial commit

---
+++ python/lesson-one/purchasingFood.py
@@ -0,0 +1,17 @@
+
+# 1) Take input from the user
+# the user will enter how many gummies they want
+# the program needs to use the value to multiply 0.50
+
+prompt = "Hey, what's up? "
+user_input = input(prompt)
+print("You said:", user_input)
+
+# 2) Display the total amount (totalNoVat)
+# the program needs to display the total amount the screen
+# this amount is the total wihtout vat
+
+#3) Display the total amount with vat applied (totalWithVat)
+# the program needs to calculate the vat of the total no vat variable
+
+
